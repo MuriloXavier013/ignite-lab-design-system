5124c3877f55274929a6f1e5832e724f61dc4737
Murilo Xavier <muriloxavier1001@gmail.com>
2022-10-22 18:30:48 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+      <span className='font-bold text-2xl text-violet-500'>Hello, world</span>
+      
+      <button className='bg-cyan-500 px-4 py-2 rounded text-white hover:bg-cyan-300 '>
+        Enviar
+      </button>
+    </>
+  )
+}
+
+export default App
---
+++ storybook-static/assets/preview.b1370cd5.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.c77c8bc0.js";import"./iframe.2add23ea.js";import"./es.map.constructor.8d37f652.js";import"./es.number.to-fixed.eab4ef1d.js";import"./index.3c237b00.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.b1370cd5.js.map
